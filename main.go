1f3416e4936968401de7e6e3f8ba82b75a4d06ac
Dhruv Dhir <dhruv@Dhruvs-MacBook-Pro.local>
2018-09-02 19:05:23 +1200

    removing todo

--- main.go
+++ main.go
@@ -119,7 +119,6 @@ func addIdea(title string, client trelloClient) (string, error) {
 }
 
 func getHelpText() string {
-	// TODO: move this string to a better place
 	helpText := "I can help you with: \n Fetching ideas - @rudolph ideas \n Fetching scheduled talks - @rudolph scheduled \n Adding an idea: @rudolph add <talk title> \n Dad joke - @rudolph make me laugh \n Help - @rudolph help \n Feature request - @dhruv <request>"
 
 	return helpText
